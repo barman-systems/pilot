c72c78dd8a28e7037e53f10325dac551c1465d8e
barmanai <alcaptain2014@gmail.com>
2026-08-26 16:43:10 +0400

    feat: attach password recovery UI to canonical PILOT app

---
+++ api/app-recovery.js
@@ -0,0 +1,36 @@
+import appHandler from './app.js';
+
+function forwardHeaders(res, headers) {
+  for (const [key, value] of Object.entries(headers)) res.setHeader(key, value);
+}
+
+export default function handler(req, res) {
+  let statusCode = 200;
+  const headers = {};
+
+  const proxy = {
+    status(code) {
+      statusCode = Number(code || 200);
+      return proxy;
+    },
+    setHeader(key, value) {
+      headers[String(key)] = value;
+      return proxy;
+    },
+    end(body = '') {
+      forwardHeaders(res, headers);
+      res.statusCode = statusCode;
+      return res.end(body);
+    },
+    send(body = '') {
+      forwardHeaders(res, headers);
+      res.statusCode = statusCode;
+      const html = typeof body === 'string'
+        ? body.replace('</body>', '<script src="/api/auth/recovery-ui"></script>\n</body>')
+        : body;
+      return res.end(html);
+    },
+  };
+
+  return appHandler(req, proxy);
+}
