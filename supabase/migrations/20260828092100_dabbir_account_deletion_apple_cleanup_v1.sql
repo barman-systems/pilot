9cb94f348f483a459352d3e9a41d145621623c80
barmanai <alcaptain2014@gmail.com>
2026-08-28 13:42:26 +0400

    DABBIR account deletion: cover inserted and updated tombstones

--- supabase/migrations/20260828092100_dabbir_account_deletion_apple_cleanup_v1.sql
+++ supabase/migrations/20260828092100_dabbir_account_deletion_apple_cleanup_v1.sql
@@ -8,7 +8,8 @@ security definer
 set search_path = pg_catalog, public, pg_temp
 as $function$
 begin
-  if new.status = 'deleted' and old.status is distinct from 'deleted' then
+  if new.status = 'deleted'
+     and (tg_op = 'INSERT' or old.status is distinct from 'deleted') then
     delete from public.dabbir_apple_entitlements where user_id = new.user_id;
   end if;
   return new;
@@ -19,7 +20,7 @@ revoke all on function dabbir_private.delete_apple_entitlement_on_account_tombst
 
 drop trigger if exists dabbir_account_delete_apple_entitlement on public.account_access_state;
 create trigger dabbir_account_delete_apple_entitlement
-after update of status on public.account_access_state
+after insert or update of status on public.account_access_state
 for each row execute function dabbir_private.delete_apple_entitlement_on_account_tombstone();
 
 comment on function dabbir_private.delete_apple_entitlement_on_account_tombstone() is
