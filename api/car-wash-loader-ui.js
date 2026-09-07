9b2c0a09a33de466f3949d8197827c1432d05878
barmanai <alcaptain2014@gmail.com>
2026-09-07 12:54:28 +0400

    fix(support): preserve car-wash loader compatibility marker

--- api/car-wash-loader-ui.js
+++ api/car-wash-loader-ui.js
@@ -98,6 +98,6 @@ export default function handler(req,res){
   if(req.method!=='GET')return res.status(405).setHeader('allow','GET').end('Method Not Allowed');
   res.setHeader('content-type','application/javascript; charset=utf-8');
   res.setHeader('cache-control','public, max-age=300');
-  res.setHeader('x-dabbir-car-wash-loader-ui','v10-support-lazy-loader');
+  res.setHeader('x-dabbir-car-wash-loader-ui','v9-market-timezone-edit');
   return res.status(200).send(script);
 }
